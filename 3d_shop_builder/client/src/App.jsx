e8f21d18d914657ae796e4fe136233a338cb38a1
hasuq33 <hardworkerarmy1234@gmail.com>
2024-06-30 16:55:39 +0530

    [IMP] 3D Shop Builder

---
+++ 3d_shop_builder/client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className='head-text'>Threee JS</h1>
+    </>
+  )
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,3 @@
+Contributr: Harshiv Joshi
+
+# This repository contains my 3d web projects
